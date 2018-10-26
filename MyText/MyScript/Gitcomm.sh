71c65f079e080cd8b868bb41f3c5f329742688db
root <593664243@qq.com>
2018-10-26 01:43:33 -0700

    shell+

--- MyText/MyScript/Gitcomm.sh
+++ MyText/MyScript/Gitcomm.sh
@@ -6,21 +6,27 @@ init() {
 echo 正在执行初始化操作...
 sleep 2
 a=$0
-if [ $a == './Gitcommi.sh' ];then
-	a=`dirname $(pwd -P)`
-	echo $a
-elif [ $a == "*/Gitcommi.sh" ];then
-	echo test
+check01=0
+check02=0
+if [ $a == './Gitcomm.sh' ];then
+	a=$(pwd -P)
+	chmod +x "$a/Gitcomm.sh"
+	check01=$?
+	rm -rf /usr/local/bin/Gitcomm
+	ln -s "$a/Gitcomm.sh" /usr/local/bin/Gitcomm
+	check02=$?
+elif [ $a == '/usr/local/bin/Gitcomm' ];then
+	location=`ls -l $(dirname $0) | grep Gitcomm | awk '{print $NF}'`
+	rm -rf /usr/local/bin/Gitcomm
+	chmod +x $location
+        check01=$?
+        ln -s $location /usr/local/bin/Gitcomm
+        check02=$?
+else
+	echo '请切换至脚本目录，使用./Gitcomm.sh init初始化'
+	exit 1
+	break
 fi
-echo $0
-chmod +x $0 2> /dev/null
-check01=$?
-#echo $check01
-rm -rf /usr/local/bin/Gitcomm
-echo $(dirname $0)
-ln -s $(dirname $0) /usr/local/bin/Gitcomm
-check02=$?
-#echo $check2
 if [[ "$check01" -eq 0 && "$check02" -eq 0 ]];then
 	echo '初始化成功，可使用"Gitcomm"命令同步'
 	exit 0
@@ -34,9 +40,20 @@ fi
 
 #commit
 commit() {
+
 location=`ls -l $(dirname $0) | grep Gitcomm | awk '{print $NF}'`
-#echo $location
 basedir=`dirname $location`
+sleep 1
+cd $basedir
+cd ../..
+echo 切换到`pwd -P`
+git add -A .
+git config --global user.email "593664243@qq.com"
+git commit
+git push
+#read -p "请输入" a
+#echo  < $a
+#echo $location
 #echo $basedir
 
 #cd $(dirname $0) && cd ../.. && echo "切换到
@@ -56,8 +73,3 @@ commit
 echo 请输入选项
 ;;
 esac
-#git add .
-#git commit
-#git push
-#read -p "请输入" a
-#echo  < $a
